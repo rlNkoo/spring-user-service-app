44f3fc22559d92edeac9c3d98ca90b67020b457e
rlNkoo <rolakartur96@gmail.com>
2024-06-17 17:07:55 +0200

    App creating started

---
+++ src/main/java/pl/rlnkoo/full_user_service/FullUserServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.full_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/rlnkoo/full_user_service/FullUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rlnkoo.full_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

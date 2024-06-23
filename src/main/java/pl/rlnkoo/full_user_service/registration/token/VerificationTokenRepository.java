9d79e6233fae569589c8095c2dc2148038a9a2a9
rlNkoo <rolakartur96@gmail.com>
2024-06-23 15:20:46 +0200

    Added Token service

--- src/main/java/pl/rlnkoo/full_user_service/registration/token/VerificationTokenRepository.java
+++ src/main/java/pl/rlnkoo/full_user_service/registration/token/VerificationTokenRepository.java
@@ -2,5 +2,9 @@ package pl.rlnkoo.full_user_service.registration.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+    Optional<VerificationToken> findByToken(String token);
 }
--- src/main/java/pl/rlnkoo/full_user_service/registration/token/VerificationTokenService.java
+++ src/main/java/pl/rlnkoo/full_user_service/registration/token/VerificationTokenService.java
@@ -1,4 +1,47 @@
 package pl.rlnkoo.full_user_service.registration.token;
 
-public class VerificationTokenService {
+import lombok.RequiredArgsConstructor;
+import lombok.var;
+import org.springframework.stereotype.Service;
+import pl.rlnkoo.full_user_service.user.User;
+import pl.rlnkoo.full_user_service.user.UserRepository;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VerificationTokenService implements IVerificationTokenService {
+
+    private final VerificationTokenRepository tokenRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public String validateToken(String token) {
+
+        Optional<VerificationToken> theToken = tokenRepository.findByToken(token);
+        if (theToken.isEmpty()) {
+            return "Invalid verification token";
+        }
+        User user = theToken.get().getUser();
+        Calendar calendar = Calendar.getInstance();
+        if (theToken.get().getExpirationTime().getTime() - calendar.getTime().getTime() <= 0) {
+            return "expired";
+        }
+        user.setEnabled(true);
+        userRepository.save(user);
+        return "valid";
+    }
+
+    @Override
+    public void saveVerifiactionTokenForUser(User user, String token) {
+        var verifiactionToken = new VerificationToken(token, user);
+        tokenRepository.save(verifiactionToken);
+
+    }
+
+    @Override
+    public Optional<VerificationToken> findByToken(String token) {
+        return tokenRepository.findByToken(token);
+    }
 }
